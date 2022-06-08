30213b3e79ab3e9787b318d10dd9f8d6c47babe6
Huy Chuong <chuonglh5798@gmail.com>
2022-06-08 15:28:14 +0700

    add origin nft route

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,2 +1,3 @@
 module.exports.user = require("./user.controller");
 module.exports.retrieveNfts = require("./retrieveNft.controller");
+module.exports.originNFT = require("./originNFT.controller");
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ const catchAsync = require("../utils/catchAsync");
 const userService = require("../services");
 
 // Create and Save a new user
-const create = catchAsync(async (req, res) => {
+const create = catchAsync(async (req, res, next) => {
   // Validate request
   if (!req.body.address) {
     res.status(400).send({ message: "Content can not be empty!" });
@@ -12,53 +12,60 @@ const create = catchAsync(async (req, res) => {
   // Create a user
   const user = await userService.createUser(req.body);
   res.status(httpStatus.CREATED).send(user);
+  next();
 });
 
 // Retrieve all users from the database.
-const findAll = catchAsync(async (req, res) => {
+const findAll = catchAsync(async (req, res, next) => {
   const listUsers = userService.getAllUsers();
   if (!user) {
     throw new ApiError(httpStatus.NOT_FOUND, "Users not found");
   }
   res.send(listUsers);
+  next();
 });
 
 // Find a single user with an id
-const findUserById = catchAsync(async (req, res) => {
+const findUserById = catchAsync(async (req, res, next) => {
   const user = await userService.getUserById(req.params.userId);
   if (!user) {
     throw new ApiError(httpStatus.NOT_FOUND, "User not found");
   }
   res.send(user);
+  next();
 });
 
 // Find a single user with an email
-const findUserByEmail = catchAsync(async (req, res) => {
+const findUserByEmail = catchAsync(async (req, res, next) => {
   const user = await userService.getUserById(req.params.email);
   if (!user) {
     throw new ApiError(httpStatus.NOT_FOUND, "User not found");
   }
   res.send(user);
+  next();
 });
 
 // Delete a user with the specified id in the request
-const deleteById = catchAsync(async (req, res) => {
+const deleteById = catchAsync(async (req, res, next) => {
   await userService.deleteUserById(req.params.userId);
   res.status(httpStatus.NO_CONTENT).send();
+  next();
 });
 
 //Query for users by conditions
-const findUsersByQuery = catchAsync(async (req, res) => {
-  const filter = pick(req.query, ['name', 'role']);
-  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+const findUsersByQuery = catchAsync(async (req, res, next) => {
+  const filter = pick(req.query, ["name", "role"]);
+  const options = pick(req.query, ["sortBy", "limit", "page"]);
   const users = await userService.queryUsers(filter, options);
   res.send(users);
+  next();
 });
 
 //Update user by id
-const updateUser = catchAsync(async (req, res) => {
+const updateUser = catchAsync(async (req, res, next) => {
   const user = await userService.updateUserById(req.params.userId, req.body);
   res.status(httpStatus.UPDATED).send(user);
+  next();
 });
 
 module.exports = {
